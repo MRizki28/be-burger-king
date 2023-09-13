79763a219cd79ec63f3eb6203d34d391ed1ea730
MRizki28 <muhammadrizkitkj123@gmail.com>
2023-09-13 16:15:46 +0800

    create update and delete data

--- app/Repositories/JenisProductRepositories.php
+++ app/Repositories/JenisProductRepositories.php
@@ -73,12 +73,12 @@ class JenisProductRepositories implements JenisProductInterfaces
 
         return response()->json([
             'generateCode' => $nextCOde
-        ],200);
+        ], 200);
     }
 
     private function getNextAvailableCode()
     {
-        $lastProduct = $this->jenisProductModel::orderBy('kode_product' , 'desc')->first();
+        $lastProduct = $this->jenisProductModel::orderBy('kode_product', 'desc')->first();
         $kodeAwal = 'JP00001';
 
         if ($lastProduct) {
@@ -89,33 +89,84 @@ class JenisProductRepositories implements JenisProductInterfaces
             $lastNumber = (int)$parts[1];
             $nextNumber = $lastNumber + 1;
 
-            $nextNumberFormat = str_pad($nextNumber, 5 ,'0' ,STR_PAD_LEFT);
+            $nextNumberFormat = str_pad($nextNumber, 5, '0', STR_PAD_LEFT);
 
             $kodeAwal = 'JP' . $nextNumberFormat;
         }
 
         return $kodeAwal;
     }
-    
+
     public function getDataById($id)
     {
         try {
-            $data = $this->jenisProductModel::where('id' , $id)->first();
+            $data = $this->jenisProductModel::where('id', $id)->first();
             if (!$data) {
                 return response()->json([
                     'message' => 'Data or id not found'
-                ],404);
-            }else{
+                ], 404);
+            } else {
                 return response()->json([
                     'message' => 'success get data by id',
                     'data' => $data
-                ],200);
+                ], 200);
             }
         } catch (\Throwable $th) {
             return response()->json([
                 'message' => 'failed',
                 'errors' => $th->getMessage()
-            ],400);
+            ], 400);
+        }
+    }
+
+    public function updateData(Request $request, $id)
+    {
+        $data = $request->all();
+
+        $validation = [
+            'kode_product' => 'required',
+            'nama_jenis_product' => 'required'
+        ];
+
+        $messageCustom = [
+            'kode_product.required' => 'Kode product wajib diisi',
+            'nama_jenis_product' => 'Jenis product wajib diisi',
+            'kode_product.unique' => 'Kode product sudah ada sebelumnya'
+        ];
+
+
+        $this->dataValidation($data, $validation, $messageCustom);
+
+        try {
+            $data = $this->jenisProductModel::where('id', $id)->first();
+            $data->kode_product = $request->input('kode_product');
+            $data->nama_jenis_product  = $request->input('nama_jenis_product');
+            $data->save();
+        } catch (\Throwable $th) {
+            return response()->json([
+                'message' => 'failed',
+                'errors' => $th->getMessage()
+            ], 400);
+        }
+
+        return response()->json([
+            'message' => 'succes update data',
+            'data' => $data
+        ], 200);
+    }
+
+    public function deleteData($id)
+    {
+        $data = $this->jenisProductModel::where('id', $id)->first();
+        if (!$data) {
+            return response()->json([
+                'message' => ' Dataor id not found'
+            ], 404);
+        } else {
+            $data->delete();
+            return response()->json([
+                'message' => 'success delete data'
+            ], 200);
         }
     }
 }
