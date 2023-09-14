03bbe5cd9dd299b9a9057a5e2e1951b11b57fb3a
MRizki28 <muhammadrizkitkj123@gmail.com>
2023-09-14 13:12:36 +0800

    update menu function

---
+++ app/Http/Controllers/CMS/MenuController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\CMS;
+
+use App\Http\Controllers\Controller;
+use App\Repositories\MenuRepositories;
+use Illuminate\Http\Request;
+
+class MenuController extends Controller
+{
+    protected $menuRepositories;
+
+    public function __construct(MenuRepositories $menuRepositories)
+    {
+        $this->menuRepositories = $menuRepositories;
+    }
+
+    public function getAllData()
+    {
+        return $this->menuRepositories->getAllData();
+    }
+}
