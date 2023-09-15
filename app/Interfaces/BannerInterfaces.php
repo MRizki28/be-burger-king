f4ec5275d30822a314424e9ee4130b7f07b0d8fc
MRizki28 <muhammadrizkitkj123@gmail.com>
2023-09-15 14:03:41 +0800

    update

--- app/Interfaces/BannerInterfaces.php
+++ app/Interfaces/BannerInterfaces.php
@@ -8,4 +8,8 @@ interface BannerInterfaces
 {
     public function createData(Request $request);
     public function getAllGambar();
+    public function getAllData();
+    public function getDataById($id);
+    public function updateData(Request $request,$id);
+    public function deleteData($id);
 }
